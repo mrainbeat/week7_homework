271a0575e67fbcf6582faea45a828d46612dcaba
윤서영 <youngyoom@yunseoyeong-ui-MacBookAir.local>
2026-05-23 01:29:07 +0900

    [Feat] 초기 리액트 환경설정 및 tailwindcss 설치

---
+++ Delivery/src/App.jsx
@@ -0,0 +1,13 @@
+import { BrowserRouter } from 'react-router-dom';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
