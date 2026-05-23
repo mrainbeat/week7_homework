ead4994b6ad163bfe6a4dc881679d75da891b15a
윤서영 <youngyoom@yunseoyeong-ui-MacBookAir.local>
2026-05-24 08:29:55 +0900

    [Mod] Navbar 구현 수정

--- Delivery/src/components/layouts/Navbar.jsx
+++ Delivery/src/components/layouts/Navbar.jsx
@@ -1,7 +1,12 @@
-const Navbar = () => {
+import cart from '../../assets/cart.svg';
+import { Link, useLocation } from 'react-router-dom';
+
+//네브바 양쪽에 들어갈 내용을 props로 관리하기
+const Navbar = ({ left, right }) => {
   return (
-    <nav className="fixed top-0 left-0 right-0 bg-[#F0485F] w-full h-[83px] flex items-center justify-between px-[40px]">
-      <div>메뉴판</div>
+    <nav className="fixed top-0 left-0 right-0 bg-[#F0485F] w-full h-[83px] flex items-center justify-between px-[40px] text-white">
+      <div>{left}</div>
+      <div className="flex gap-[38px] items-center">{right}</div>
     </nav>
   );
 };
--- Delivery/src/pages/Menu.jsx
+++ Delivery/src/pages/Menu.jsx
@@ -1,5 +1,24 @@
+import Navbar from '../components/layouts/Navbar';
+import cart from '../assets/cart.svg';
+import { Link, useLocation } from 'react-router-dom';
+
 const Menu = () => {
-  return <div>Menu</div>;
+  return (
+    <div>
+      <Navbar
+        left={<span className="text-2xl">장바구니</span>}
+        right={
+          <>
+            <img src={cart} alt="장바구니" />
+            <Link to="/Login" className="hover:text-black">
+              {' '}
+              로그아웃
+            </Link>
+          </>
+        }
+      />
+    </div>
+  );
 };
 
 export default Menu;
