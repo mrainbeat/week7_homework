e3a514965f692008fc27b48191ceaa79fe6749dc
윤서영 <youngyoom@yunseoyeong-ui-MacBookAir.local>
2026-05-24 08:55:33 +0900

    [Mod]  Order페이지 Navbar 적용

--- Delivery/src/pages/Menu.jsx
+++ Delivery/src/pages/Menu.jsx
@@ -6,15 +6,15 @@ const Menu = () => {
   return (
     <div>
       <Navbar
-        left={<span className="text-2xl">장바구니</span>}
+        left={<span className="text-[36px] font-bold">장바구니</span>}
         right={
-          <>
+          <div className="flex gap-[38px] items-center">
             <img src={cart} alt="장바구니" />
             <Link to="/Login" className="hover:text-black">
               {' '}
               로그아웃
             </Link>
-          </>
+          </div>
         }
       />
     </div>
